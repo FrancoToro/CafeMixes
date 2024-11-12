3f93f2415edab5e0576cc9d632d7849e49c88964
FrancoToro <ftoro19@alumnos.utalca.cl>
2024-11-11 23:24:07 -0300

    actualizacion 0.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 import 'pages/my_home_page.dart'; 
 
-void main() {
+import 'dart:async';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+void main() async
+{
+  //iniciar y/o abrir la base de datos
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final database = openDatabase(
+  join(await getDatabasesPath(),"coffee_db"),
+  onCreate: (db, version) {
+    return db.execute("CREATE TABLE recipes(id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, descripcion TEXT, ingredientes TEXT, pasos TEXT, favorita INTEGER)");
+  }, version: 1,
+  );
+
   runApp(const MyApp());
 }
 
--- lib/model/Receta.dart
+++ lib/model/Receta.dart
@@ -1,11 +1,30 @@
 
 class Receta {
+  late int id;
   late String nombre;
   late String descripcion;
-  late List<String> ingredientes;
-  late List<String> pasos;
+  late String ingredientes;
+  late String pasos;
   late bool favorita;
   late String imagen;
+
+  //crear version para meter a base de datos
+  Map<String, Object?> toMap()
+  {
+    return {
+      'id': id,
+      'nombre': nombre,
+      'descripcion': descripcion,
+      'ingredientes': ingredientes,
+      'pasos': pasos,
+      'favorita': favorita ? 1 : 0
+    };
+  }
+
+  Receta()
+  {
+    
+  }
 
   void agregarIngrediente(String ingrediente) {
   }
